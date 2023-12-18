bc144a7193a00e4751e8a979e57c53b8d2e40436
oksid <oksid123123@gmail.com>
2023-12-18 22:07:40 +0400

    Add ITypeOfProduct Interfaces

---
+++ FishingShop/FishingShop.Services/Interfaces/ITypeOfProductService.cs
@@ -0,0 +1,17 @@
+﻿using FishingShop.WebApi.Models;
+
+namespace FishingShop.Services.Interfaces
+{
+    public interface ITypeOfProductService
+    {
+        public Task<IEnumerable<TypeOfProduct>> GetTypeOfProductsAsync();
+
+        public Task<TypeOfProduct> GetTypeOfProductAsync(int typeId);
+
+        public Task<bool> UpdateTypeOfProductAsync(TypeOfProduct typeOfProduct);
+
+        public Task<bool> RemoveTypeOfProductAsync(int typeId);
+
+        public Task<bool> CreateTypeOfProductAsync(TypeOfProduct typeOfProduct);
+    }
+}
--- FishingShop/FishingShop.WebApi.Models/Product.cs
+++ FishingShop/FishingShop.WebApi.Models/Product.cs
@@ -8,7 +8,7 @@
 
         public string Description { get; set; }
 
-        public int TypeOfProductId {  get; set; }
+        public int TypeOfProductId { get; set; } = 0;
 
         public TypeOfProduct TypeOfProduct { get; set; } = null!;
 

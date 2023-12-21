fe51ff467ff8f1ea685fda666d8ecc6727da33ae
oksid <oksid123123@gmail.com>
2023-12-21 19:25:48 +0400

    Create IProductService interface

---
+++ FishingShop/FishingShop.Services/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using FishingShop.WebApi.Models;
+
+namespace FishingShop.Services.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<Product>> GetAllProductsAsync();
+
+        public Task<Product> GetProductAsync(int productId);
+
+        public Task<bool> CreateProductAsync(Product product);
+
+        public Task<bool> UpdateProductAsync(Product product);
+
+        public Task<bool> DeleteProductAsync(Product product);
+    }
+}
